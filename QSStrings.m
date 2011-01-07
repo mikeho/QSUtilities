1ab89399565da41cfb4101a42a93f78ebfcfdd54
Mike Ho <mike@quasidea.com>
2011-01-07 14:55:28 -0800

    Initial Set of Cleanups

--- QSStrings.m
+++ QSStrings.m
@@ -1,16 +1,33 @@
-//
-//  Strings.m
-//  iVQ
-//
-//  Created by Mike Ho on 10/8/10.
-//  Copyright 2010 Quasidea Development, LLC. All rights reserved.
-//
+/**
+ * QSStrings.m
+ * 
+ * Copyright (c) 2010 - 2011, Quasidea Development, LLC
+ * For more information, please go to http://www.quasidea.com/
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-#import "Strings.h"
+#import "QSUtilities.h"
 #import "GTMNSString+HTML.h"
 
 
-@implementation Strings
+@implementation QSStrings
 
 + (NSString *)implodeObjectArray:(NSArray *)objArray WithSelector:(SEL)selSelector Glue:(NSString *)strGlue {
 	NSInteger intCount = [objArray count];
